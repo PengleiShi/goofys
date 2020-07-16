a88bea426bc38107a32a1b41870f644b9106752a
shipenglei24@163.com <shipenglei24@163.com>
2020-07-16 16:56:43 +0800

    fix

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ package main
 import (
 	goofys "github.com/kahing/goofys/api"
 	. "github.com/kahing/goofys/api/common"
-	. "github.com/kahing/goofys/internal"
+	. "github.com/PengleiShi/goofys/internal"
 
 	"fmt"
 	"os"
