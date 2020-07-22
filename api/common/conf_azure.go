0036fba23bbb9a90870c347512531606fd093880
shipenglei24@163.com <shipenglei24@163.com>
2020-07-22 21:44:38 +0800

    tmp4

--- api/common/conf_azure.go
+++ api/common/conf_azure.go
@@ -179,6 +179,10 @@ func (c AzureAuthorizerConfig) Authorizer() (autorest.Authorizer, error) {
 	}
 
 	if cred, err := env.GetClientCredentials(); err == nil {
+		log.Fatalf("Mounting file system: %v", cred.TenantID)
+		log.Fatalf("Mounting file system: %v", cred.AADEndpoint)
+		log.Fatalf("Mounting file system: %v", cred.ClientID)
+		log.Fatalf("Mounting file system: %v", cred.ClientSecret)
 		if authorizer, err := cred.Authorizer(); err == nil {
 			return authorizer, err
 		}
